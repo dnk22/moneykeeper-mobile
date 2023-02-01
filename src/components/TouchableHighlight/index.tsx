4846a8a2c2db41bb2fb0115369ede68ed9386b66
Duy Nguyen <duynk198@gmail.com>
2023-02-01 23:57:33 +0700

    feat : update TouchableHighlightComponent

--- src/components/TouchableHighlight/index.tsx
+++ src/components/TouchableHighlight/index.tsx
@@ -1,23 +1,27 @@
 import { memo } from 'react';
 import isEqual from 'react-fast-compare';
-import { TouchableHighlight, TouchableHighlightProps } from 'react-native';
+import { TouchableHighlight, TouchableHighlightProps, View } from 'react-native';
 import { useCustomTheme } from 'resources/theme';
 
+type TouchableHighlightComponent = TouchableHighlightProps & {
+  isActive?: boolean;
+};
 function TouchableHighlightComponent({
   style,
+  isActive = true,
   children,
   activeOpacity = 0.8,
   onPress,
-}: TouchableHighlightProps) {
+}: TouchableHighlightComponent) {
   const { colors } = useCustomTheme();
   return (
     <TouchableHighlight
       style={[{ borderRadius: 10 }, style]}
-      onPress={onPress}
+      onPress={isActive ? onPress : undefined}
       underlayColor={colors.background}
       activeOpacity={activeOpacity}
     >
-      {children}
+      <View style={{ opacity: isActive ? 1 : 0.5 }}>{children}</View>
     </TouchableHighlight>
   );
 }
