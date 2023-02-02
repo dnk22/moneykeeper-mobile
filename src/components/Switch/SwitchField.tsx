fb11632ef1d4e68527b6858f4bc0117c959f5483
Duy Nguyen <duynk198@gmail.com>
2023-02-02 23:35:34 +0700

    feat : update switch component

--- src/components/Switch/SwitchField.tsx
+++ src/components/Switch/SwitchField.tsx
@@ -1,16 +1,34 @@
 import React, { memo } from 'react';
 import isEqual from 'react-fast-compare';
 import { useController } from 'react-hook-form';
-import Switch from '.';
 import { ISwitchFieldProps } from './type';
+import { Switch as RNSwitch } from 'react-native';
+import { useCustomTheme } from 'resources/theme';
 
-function SwitchField({ name, control, ...rest }: ISwitchFieldProps) {
+function SwitchField({
+  name,
+  control,
+  style,
+  ios_backgroundColor,
+  trackColor,
+  ...rest
+}: ISwitchFieldProps) {
+  const { colors } = useCustomTheme();
   const {
     field: { value, onChange },
   } = useController({
     name,
     control,
   });
-  return <Switch onValueChange={onChange} value={value} {...rest} />;
+  return (
+    <RNSwitch
+      ios_backgroundColor={ios_backgroundColor || '#3e3e3e'}
+      trackColor={trackColor || { true: colors.primary }}
+      onValueChange={onChange}
+      style={[style]}
+      value={value}
+      {...rest}
+    />
+  );
 }
 export default memo(SwitchField, isEqual);
