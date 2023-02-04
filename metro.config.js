166ef4ce2da681d0743928b7056b7fad442f2859
Duy Nguyen <duynk198@gmail.com>
2023-02-04 11:16:12 +0700

    depend : add react native fast image

--- metro.config.js
+++ metro.config.js
@@ -5,24 +5,6 @@
  * @format
  */
 
-// module.exports = {
-//   transformer: {
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: true,
-//       },
-//     }),
-//   },
-// };
-
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
