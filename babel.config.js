6931635453f09a96ae6bcb16a837b846aebb992b
Duy Nguyen <duynk198@gmail.com>
2023-02-22 21:32:24 +0700

    depend : add watermelonDB

--- babel.config.js
+++ babel.config.js
@@ -21,5 +21,6 @@ module.exports = {
       },
     ],
     'react-native-reanimated/plugin',
+    ["@babel/plugin-proposal-decorators", { "legacy": true }]
   ],
 };
---
+++ ios/wmelon.swift
@@ -0,0 +1,8 @@
+//
+//  wmelon.swift
+//  moneykeeper
+//
+//  Created by Duy Nguyen on 22/02/2023.
+//
+
+import Foundation
