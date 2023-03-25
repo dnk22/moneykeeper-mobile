ee9013bddd3b14f01eb191749f472e8f9e636fff
Duy Nguyen <duynk198@gmail.com>
2023-03-25 23:30:13 +0700

    chore : add gen images command

--- updateImagesPath.js
+++ updateImagesPath.js
@@ -1,5 +1,28 @@
 const fs = require('fs');
-const files = fs.readdirSync('./src/assets/images');
-const ex = '{\n' + files.map((x) => `"${x.split('.png')[0]}": require("./${x}"),`).join('\n') + '}';
-const res = 'export default ' + ex;
-fs.writeFileSync('./src/assets/images/index.js', res);
+const IMAGE_PATH = './src/assets/images';
+let result = [];
+const imagesPath = getDirectories(IMAGE_PATH);
+
+function getDirectories(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + '/' + file).isDirectory();
+  });
+}
+
+for (const item of imagesPath) {
+  const files = fs.readdirSync(`${IMAGE_PATH}/${item}`);
+  const ex =
+    '\n' +
+    files
+      .map((x, index) => {
+        const decimal = files.length - 1 === index ? '' : ',';
+        return `${x.split('.png')[0]}: require("./${item}/${x}")${decimal}`;
+      })
+      .join('\n');
+  result.push(ex);
+}
+const formatResult = result.flat((item) => item);
+const res = `export const imagesPath = {
+    ${formatResult}
+}`;
+fs.writeFileSync(`${IMAGE_PATH}/index.js`, res);
